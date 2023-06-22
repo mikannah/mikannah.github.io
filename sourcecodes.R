f624131366f013d516ebe440d86a12fba782300b
mikannah <hannakimofficial@gmail.com>
2023-06-22 10:20:34 -0500

    Initial commit

---
+++ sourcecodes.R
@@ -0,0 +1,6 @@
+## Codes for creating my website with Quarto + R Studio
+## https://quarto.org/docs/websites/
+## https://ucsb-meds.github.io/creating-quarto-websites/
+
+library(usethis)
+usethis::use_git()
